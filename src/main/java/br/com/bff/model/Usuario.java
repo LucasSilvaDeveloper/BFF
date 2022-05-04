111cd9f371137c48e7ac3c941eb3fc70b8e6b7e5
Lucas de Oliveira da Silva <lucas@inventfy.com.br>
2022-05-03 21:33:50 -0300

    tarefa da classe de usuario

---
+++ src/main/java/br/com/bff/model/Usuario.java
@@ -0,0 +1,9 @@
+package br.com.bff.model;
+
+public class Usuario {
+
+    //FIXME add atributos apra o usuario
+    // nome, data de cadastro, data de nascimento, cpf, email, telefone, data de atualização, id
+    // criar notações de geet e set build to string usando o lombok
+
+}
