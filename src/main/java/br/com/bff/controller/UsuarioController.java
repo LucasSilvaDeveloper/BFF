ae9263be39389155fcbeaa70217ec4e674c99ab0
Lucas de Oliveira da Silva <lucas@inventfy.com.br>
2022-05-21 21:30:29 -0300

    update crud usuario

--- src/main/java/br/com/bff/controller/UsuarioController.java
+++ src/main/java/br/com/bff/controller/UsuarioController.java
@@ -1,18 +1,24 @@
 package br.com.bff.controller;
 
-import br.com.bff.model.Usuario;
 import br.com.bff.model.dto.UsuarioRequestDTO;
 import br.com.bff.service.UsuarioService;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+@Api(tags = "CRUD de usuario", produces = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
 @RequestMapping("/usuario")
 @RestController
@@ -26,4 +32,22 @@ public class UsuarioController {
         return usuarioService.save(usuarioRequestDTO);
     }
 
+    @ApiOperation("Deletar usuario por id")
+    @DeleteMapping
+    public ResponseEntity deleteById(@PathVariable Long id){
+        return usuarioService.deleteById(id);
+    }
+
+    @ApiOperation("Atualizar usuario")
+    @PutMapping("/{id}")
+    public ResponseEntity update(@Valid @RequestBody UsuarioRequestDTO usuarioRequestDTO,
+                                 @PathVariable Long id){
+        return usuarioService.update(usuarioRequestDTO, id);
+    }
+
+    @ApiOperation("Buscar todos os usuario")
+    @GetMapping
+    public ResponseEntity findAll(){
+        return usuarioService.findAll();
+    }
 }
--- src/main/java/br/com/bff/service/UsuarioService.java
+++ src/main/java/br/com/bff/service/UsuarioService.java
@@ -1,15 +1,17 @@
 package br.com.bff.service;
 
-import br.com.bff.config.ModelMapperConfig;
 import br.com.bff.model.Usuario;
 import br.com.bff.model.dto.UsuarioRequestDTO;
 import br.com.bff.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -19,8 +21,40 @@ public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
     private final ModelMapper modelMapper;
 
-    public ResponseEntity save(UsuarioRequestDTO usuarioRequestDTO){
-        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioRepository.save(modelMapper.map(usuarioRequestDTO, Usuario.class)));
+    public ResponseEntity save(UsuarioRequestDTO usuarioRequestDTO) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(usuarioRepository
+                        .save(modelMapper.map(usuarioRequestDTO, Usuario.class)));
     }
 
+    public ResponseEntity deleteById(Long id) {
+        Optional<Usuario> usuario = usuarioRepository.findById(id);
+
+        return usuario.isPresent() ?
+                ResponseEntity.status(HttpStatus.OK).body(usuario.get()) :
+                ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario não encontrado");
+
+    }
+
+    public ResponseEntity update(UsuarioRequestDTO usuarioRequestDTO, Long id){
+        var usuarioAUX = usuarioRepository.findById(id);
+
+        if (usuarioAUX.isPresent()){
+            BeanUtils.copyProperties(usuarioRequestDTO, usuarioAUX.get(), "id");
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(usuarioRepository.save(usuarioAUX.get()));
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario não encontrado");
+        }
+    }
+
+    public ResponseEntity findAll(){
+        var usuarios = usuarioRepository.findAll();
+
+        return usuarios.isEmpty() ?
+                ResponseEntity.noContent().build() :
+                ResponseEntity.status(HttpStatus.OK).body(usuarios);
+    }
 }
