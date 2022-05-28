222421a802518b48be2b5bf8f4e428d9640184ef
Lucas de Oliveira da Silva <lucas@inventfy.com.br>
2022-05-28 15:03:56 -0300

    criação da classe de constantes

---
+++ src/main/java/br/com/bff/util/Constants.java
@@ -0,0 +1,16 @@
+package br.com.bff.util;
+
+public class Constants {
+
+    private Constants() {
+    }
+
+    public static final String ID = "id";
+    public static final String NOME = "Nome";
+    public static final String CPF = "CPF";
+    public static final String EMAIL = "Email";
+    public static final String TELEFONE = "Telefone";
+    public static final String DATA_ATUALIZACAO = "DataAtualizacao";
+    public static final String DATA_CADASTRO = "DataCadastro";
+    public static final String TODAS_COLUNAS = "Todas as colunas";
+}
